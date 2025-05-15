e3f3d14a25e6e87919c2706db75fdac0538c0ef1
Angel-alvar <al22760230@ite.edu.mx>
2025-05-15 12:49:10 -0700

    Update temasasesoria.php

--- app/Models/temasasesoria.php
+++ app/Models/temasasesoria.php
@@ -4,7 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class temasasesoria extends Model
+class TemasAsesoria extends Model
 {
-    //
+    protected $table = 'temas_asesoria';
+    protected $primaryKey = 'ID_Tema';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Nombre_Tema',
+        'Materia_Tema',
+        'PalClav_Temas',
+    ];
+
+    public function asesorias()
+    {
+        return $this->hasMany(Asesoria::class, 'idTemaAseso', 'ID_Tema');
+    }
 }
