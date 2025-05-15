75992579ae0f8c818b3c003435112b7bd8bc943c
Angel-alvar <al22760230@ite.edu.mx>
2025-05-15 12:49:34 -0700

    Update asesoria.php

--- app/Models/asesoria.php
+++ app/Models/asesoria.php
@@ -4,7 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class asesoria extends Model
+class Asesoria extends Model
 {
-    //
+    protected $table = 'asesoria';
+    protected $primaryKey = 'ID_Asesoria';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Fecha_Aseso',
+        'Hora_Aseso',
+        'Modalidad',
+        'idAsesor',
+        'idAsesorado',
+        'idTemaAseso',
+    ];
+
+    public function asesor()
+    {
+        return $this->belongsTo(Asesor::class, 'idAsesor', 'ID_Asesor');
+    }
+
+    public function asesorado()
+    {
+        return $this->belongsTo(Asesorado::class, 'idAsesorado', 'ID_Asesorado');
+    }
+
+    public function tema()
+    {
+        return $this->belongsTo(TemasAsesoria::class, 'idTemaAseso', 'ID_Tema');
+    }
 }
