6800025f750d0558ab033dfa58795f868fd1fa3b
Angel-alvar <al22760230@ite.edu.mx>
2025-05-15 12:48:49 -0700

    Update asesorado.php

--- app/Models/asesorado.php
+++ app/Models/asesorado.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class asesorado extends Model
+class Asesorado extends Model
 {
-    //
+    protected $table = 'asesorado';
+    protected $primaryKey = 'ID_Asesorado';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'Nombre_Asesorado',
+        'NoControl',
+        'Correo_Ins',
+        'Password',
+        'Carrera',
+    ];
+
+    public function asesorias()
+    {
+        return $this->hasMany(Asesoria::class, 'idAsesorado', 'ID_Asesorado');
+    }
 }
